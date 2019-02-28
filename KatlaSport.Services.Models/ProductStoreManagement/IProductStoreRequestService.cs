2ca23e1a1772a586a28152e8e0e2cbbb19342fdc
Vadim Makarchik <makarchikvadim@gmail.com>
2019-02-28 03:48:29 +0300

    Add IProductStoreRequest and updating request.

---
+++ KatlaSport.Services.Models/ProductStoreManagement/IProductStoreRequestService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace KatlaSport.Services.ProductStoreManagement
+{
+    /// <summary>
+    /// Represent a product store request service
+    /// </summary>
+    public interface IProductStoreRequestService
+    {
+        /// <summary>
+        /// Create request.
+        /// </summary>
+        /// <param name="updateRequest">Updating request</param>
+        /// <returns><see cref="Task"/>.</returns>
+        Task CreateRequest(UpdateRequest updateRequest);
+    }
+}
---
+++ KatlaSport.Services.Models/ProductStoreManagement/UpdateRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace KatlaSport.Services.ProductStoreManagement
+{
+    /// <summary>
+    /// Represents a request for creating and updating a request.
+    /// </summary>
+    public class UpdateRequest
+    {
+        /// <summary>
+        /// Gets or sets product store id.
+        /// </summary>
+        public int ProductStoreId { get; set; }
+
+        /// <summary>
+        /// Gets or sets additional product amount.
+        /// </summary>
+        public int Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets ...
+        /// </summary>
+        public bool IsCancelled { get; set; }
+    }
+}
