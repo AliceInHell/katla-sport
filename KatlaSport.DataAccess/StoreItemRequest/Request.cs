0549f9e85c8acc795d0eb21ddb467c6a78ea9e10
Vadim Makarchik <makarchikvadim@gmail.com>
2019-02-28 03:18:51 +0300

    Add Request-Init migration.

---
+++ KatlaSport.DataAccess/StoreItemRequest/Request.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KatlaSport.DataAccess.StoreItemRequest
+{
+    /// <summary>
+    /// Represents a request for add some products.
+    /// </summary>
+    public class Request
+    {
+        /// <summary>
+        /// Gets or sets request id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets product store id.
+        /// </summary>
+        public int ProductStoreId { get; set; }
+
+        /// <summary>
+        /// Gets or sets request user id.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets request time.
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets additional product amount.
+        /// </summary>
+        public int Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets ...
+        /// </summary>
+        public bool IsCancelled { get; set; }
+    }
+}
---
+++ KatlaSport.DataAccess/StoreItemRequest/RequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace KatlaSport.DataAccess.StoreItemRequest
+{
+    public class RequestConfiguration : EntityTypeConfiguration<Request>
+    {
+        public RequestConfiguration()
+        {
+            ToTable("store_item_request");
+            HasKey(i => i.Id);
+            Property(i => i.UserId).HasColumnName("user_id");
+            Property(i => i.ProductStoreId).HasColumnName("product_store_id");
+            Property(i => i.Time).HasColumnName("time");
+            Property(i => i.Amount).HasColumnName("amount");
+            Property(i => i.IsCancelled).HasColumnName("is_cancelled");
+        }
+    }
+}
